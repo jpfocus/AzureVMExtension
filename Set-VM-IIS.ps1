893f6bb8145bfb67786237447a195cd960cb100d
mikebai <mikebai604@gmail.com>
2021-03-28 18:43:54 +0900

    Update Set-VM-IIS.ps1

--- Set-VM-IIS.ps1
+++ Set-VM-IIS.ps1
@@ -1,7 +1,7 @@
-Import-Module ServerManager
-Add-WindowsFeature Web-Server
-Add-WindowsFeature NET-Framework-Features, NET-Framework-Core
-Install-WindowsFeature Web-Server, Web-WebServer, Web-Common-Http, Web-Default-Doc, Web-Dir-Browsing, Web-Http-Errors, Web-Static-Content, Web-Health, Web-Http-Logging, Web-Performance, Web-Stat-Compression, Web-Security, Web-Filtering, Web-App-Dev, Web-Net-Ext, Web-Net-Ext45, Web-Asp, Web-Asp-Net, Web-Asp-Net45, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Mgmt-Tools, Web-Mgmt-Console, Web-Mgmt-Compat, Web-Metabase, Web-Lgcy-Mgmt-Console, Web-Lgcy-Scripting, Web-WMI –IncludeManagementTools
+Import-Module ServerManager
+Add-WindowsFeature Web-Server
+Add-WindowsFeature NET-Framework-Features, NET-Framework-Core
+Install-WindowsFeature Web-Server, Web-WebServer, Web-Common-Http, Web-Default-Doc, Web-Dir-Browsing, Web-Http-Errors, Web-Static-Content, Web-Health, Web-Http-Logging, Web-Performance, Web-Stat-Compression, Web-Security, Web-Filtering, Web-App-Dev, Web-Net-Ext, Web-Net-Ext45, Web-Asp, Web-Asp-Net, Web-Asp-Net45, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Mgmt-Tools, Web-Mgmt-Console, Web-Mgmt-Compat, Web-Metabase, Web-Lgcy-Mgmt-Console, Web-Lgcy-Scripting, Web-WMI –IncludeManagementTools
 
 New-WebBinding -Name "Default Web Site" -IPAddress "*" -Port 8081 -HostHeader ""
 New-WebBinding -Name "Default Web Site" -IPAddress "*" -Port 8082 -HostHeader ""
@@ -9,13 +9,24 @@ New-WebBinding -Name "Default Web Site" -IPAddress "*" -Port 8082 -HostHeader ""
 New-NetFirewallRule -DisplayName "iis-8081" -Direction Inbound -LocalPort 8081 -Protocol TCP -Action Allow
 New-NetFirewallRule -DisplayName "iis-8082" -Direction Inbound -LocalPort 8082 -Protocol TCP -Action Allow
 
-Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
+Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
 
-New-Item -ItemType directory -Path "C:\inetpub\wwwroot\images"
-New-Item -ItemType directory -Path "C:\inetpub\wwwroot\video"
+$folder_img = "C:\inetpub\wwwroot\images"
+if (-not (Test-Path $folder_img)) {
+    New-Item -ItemType directory -Path $folder_img
+}
+$folder_video = "C:\inetpub\wwwroot\video"
+if (-not (Test-Path $folder_video)) {
+    New-Item -ItemType directory -Path $folder_video
+}
 
-$imagevalue = "Images: " + $($env:computername)
-Add-Content -Path "C:\inetpub\wwwroot\images\test.htm" -Value $imagevalue
+$imagevalue = "Images: " + $($env:computername)
+$imageHtmlPath = "$folder_img\test.htm" 
+# Add-Content -Path $imageHtmlPath -Value $imagevalue
+$imagevalue | Out-String | Out-File $imageHtmlPath -Encoding utf8 
 
-$videovalue = "Video: " + $($env:computername)
-Add-Content -Path "C:\inetpub\wwwroot\video\test.htm" -Value $videovalue    
+
+$videovalue = "Video: " + $($env:computername)
+$videoHtmlPath = "$folder_video\test.htm" 
+# Add-Content -Path "$folder_video\test.htm" -Value $videovalue
+$videovalue | Out-String | Out-File $videoHtmlPath -Encoding utf8
