556b5d947a5cc7d29631900f8a37ace50cf2fe96
mikebai <mikebai604@gmail.com>
2022-10-23 20:56:51 +0900

    Update set-iis-and-firewal.ps1

--- set-iis-and-firewal.ps1
+++ set-iis-and-firewal.ps1
@@ -1,4 +1,4 @@
-﻿Import-Module ServerManager
+Import-Module ServerManager
 Add-WindowsFeature Web-Server
 Add-WindowsFeature NET-Framework-Features, NET-Framework-Core
 Install-WindowsFeature Web-Server, Web-WebServer, Web-Common-Http, Web-Default-Doc, Web-Dir-Browsing, Web-Http-Errors, Web-Static-Content, Web-Health, Web-Http-Logging, Web-Performance, Web-Stat-Compression, Web-Security, Web-Filtering, Web-App-Dev, Web-Net-Ext, Web-Net-Ext45, Web-Asp, Web-Asp-Net, Web-Asp-Net45, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Mgmt-Tools, Web-Mgmt-Console, Web-Mgmt-Compat, Web-Metabase, Web-Lgcy-Mgmt-Console, Web-Lgcy-Scripting, Web-WMI –IncludeManagementTools
@@ -30,22 +30,47 @@ if ($null -eq $netFirewallRule2) {
 # create index page
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
 
-$folder_img = "C:\inetpub\wwwroot\images"
-if (-not (Test-Path $folder_img)) { New-Item -ItemType directory -Path $folder_img }
-"Images: " + $($env:computername) | Out-String | Out-File "$folder_img\test.htm"  -Encoding utf8 
+
+$rootFolder = "C:\inetpub\wwwroot"
+
+$curFolderName = "images"
+$curFolder = "$($rootFolder)\$($curFolderName)"
+if (-not (Test-Path $curFolder)) { New-Item -ItemType directory -Path $curFolder }
+"$($curFolderName): " + $($env:computername) | Out-String | Out-File "$curFolder\test.htm" -Encoding utf8 
+
+$curFolderName = "videos"
+$curFolder = "$($rootFolder)\$($curFolderName)"
+if (-not (Test-Path $curFolder)) { New-Item -ItemType directory -Path $curFolder }
+"$($curFolderName): " + $($env:computername) | Out-String | Out-File "$curFolder\test.htm" -Encoding utf8 
+
+$curFolderName = "folder1"
+$curFolder = "$($rootFolder)\$($curFolderName)"
+if (-not (Test-Path $curFolder)) { New-Item -ItemType directory -Path $curFolder }
+"$($curFolderName): " + $($env:computername) | Out-String | Out-File "$curFolder\test.htm" -Encoding utf8 
+
+$curFolderName = "folder2"
+$curFolder = "$($rootFolder)\$($curFolderName)"
+if (-not (Test-Path $curFolder)) { New-Item -ItemType directory -Path $curFolder }
+"$($curFolderName): " + $($env:computername) | Out-String | Out-File "$curFolder\test.htm" -Encoding utf8 
 
 
-$folder_video = "C:\inetpub\wwwroot\videos"
-if (-not (Test-Path $folder_video)) { New-Item -ItemType directory -Path $folder_video }
-"Videos: " + $($env:computername) | Out-String | Out-File "$folder_videos\test.htm"  -Encoding utf8 
+
+# $folder_img = "C:\inetpub\wwwroot\images"
+# $curFolder = $folder_img
+# if (-not (Test-Path $curFolder)) { New-Item -ItemType directory -Path $curFolder }
+# "Images: " + $($env:computername) | Out-String | Out-File "$curFolder\test.htm"  -Encoding utf8 
 
 
-$folder1 = "C:\inetpub\wwwroot\folder1"
-if (-not (Test-Path $folder1)) { New-Item -ItemType directory -Path $folder1 }
-"$folder1<br>" + $($env:computername) | Out-String | Out-File "$folder1\test.htm" -Encoding utf8 
+# $folder_videos = "C:\inetpub\wwwroot\videos"
+# if (-not (Test-Path $folder_videos)) { New-Item -ItemType directory -Path $folder_videos }
+# "Videos: " + $($env:computername) | Out-String | Out-File "$folder_videos\test.htm"  -Encoding utf8 
 
 
-$folder2 = "C:\inetpub\wwwroot\folder2"
-if (-not (Test-Path $folder2)) { New-Item -ItemType directory -Path $folder2 }
-"$folder2<br>" + $($env:computername) | Out-String | Out-File "$folder2\test.htm" -Encoding utf8 
+# $folder1 = "C:\inetpub\wwwroot\folder1"
+# if (-not (Test-Path $folder1)) { New-Item -ItemType directory -Path $folder1 }
+# "$folder1<br>" + $($env:computername) | Out-String | Out-File "$folder1\test.htm" -Encoding utf8 
 
+
+# $folder2 = "C:\inetpub\wwwroot\folder2"
+# if (-not (Test-Path $folder2)) { New-Item -ItemType directory -Path $folder2 }
+# "$folder2<br>" + $($env:computername) | Out-String | Out-File "$folder2\test.htm" -Encoding utf8 
